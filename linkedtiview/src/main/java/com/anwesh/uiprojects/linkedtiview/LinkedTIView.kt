bb362f534493b1837a5f2d534c7a0bf6a8c140af
Anwesh43 <anweshthecool0@gmail.com>
2018-06-19 11:09:37 +0530

    adding LinkedIT class

--- linkedtiview/src/main/java/com/anwesh/uiprojects/linkedtiview/LinkedTIView.kt
+++ linkedtiview/src/main/java/com/anwesh/uiprojects/linkedtiview/LinkedTIView.kt
@@ -140,4 +140,28 @@ class LinkedTIView (ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LinkedIT(var i : Int) {
+
+        private var curr : ITNode = ITNode(0)
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(stopcb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                stopcb(it)
+            }
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            curr.startUpdating(startcb)
+        }
+    }
 }
