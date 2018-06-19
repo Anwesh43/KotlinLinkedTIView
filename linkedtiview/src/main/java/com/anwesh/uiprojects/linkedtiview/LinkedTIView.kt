d0d229685c138925ccee4f005e1844cd59b039c7
Anwesh43 <anweshthecool0@gmail.com>
2018-06-19 10:10:50 +0530

    adding onTocuhEvent in LinkedTIView

--- linkedtiview/src/main/java/com/anwesh/uiprojects/linkedtiview/LinkedTIView.kt
+++ linkedtiview/src/main/java/com/anwesh/uiprojects/linkedtiview/LinkedTIView.kt
@@ -14,6 +14,15 @@ class LinkedTIView (ctx : Context) : View(ctx) {
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     override fun onDraw(canvas : Canvas) {
-        
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                
+            }
+        }
+        return true
     }
 }
