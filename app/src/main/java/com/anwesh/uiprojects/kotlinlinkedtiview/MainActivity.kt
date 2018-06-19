6dec1e33c4d866c4380ec119af1230cae71a4ee1
Anwesh43 <anweshthecool0@gmail.com>
2018-06-19 11:22:59 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedtiview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedtiview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.kotlinlinkedtiview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linkedtiview.LinkedTIView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LinkedTIView.create(this)
     }
 }
