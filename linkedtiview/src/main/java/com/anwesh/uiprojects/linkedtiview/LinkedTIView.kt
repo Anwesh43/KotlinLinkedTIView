e390c296a833522fe27513ae1a7e3683cf00672c
Anwesh43 <anweshthecool0@gmail.com>
2018-06-19 10:08:26 +0530

    adding Linkedtiview module

---
+++ linkedtiview/src/main/java/com/anwesh/uiprojects/linkedtiview/LinkedTIView.kt
@@ -0,0 +1,19 @@
+package com.anwesh.uiprojects.linkedtiview
+
+/**
+ * Created by anweshmishra on 19/06/18.
+ */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.*
+
+class LinkedTIView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+        
+    }
+}
